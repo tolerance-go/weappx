b8836980750683150e9cd7d53ae0ce5a1077cf47
bzone <yarnbCoder@gmail.com>
2018-05-08 13:47:53 +0800

    fix: states[k].call(_this) wepy-redux的lib没有构建就发布了

--- packages/wepy-weappx/src/index.js
+++ packages/wepy-weappx/src/index.js
@@ -1,5 +1,13 @@
-import { connect, setStore, getStore } from 'wepy-redux';
+import connect from './connect';
+import { setStore, getStore } from './store';
+import { mapState, mapActions } from './helpers';
 
-const connector = { connect, setStore, getStore };
+const connector = {
+  connect,
+  setStore,
+  getStore,
+  mapState,
+  mapActions,
+};
 
 export default connector;
